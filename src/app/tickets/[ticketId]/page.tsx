884b76ce0132b2303470dd871408d31fc9b4b28a
Madhu Dadi <madhu.kumar245@gmail.com>
2026-03-23 16:37:23 +0530

    feat: initial commit

---
+++ src/app/tickets/[ticketId]/page.tsx
@@ -0,0 +1,32 @@
+import {initialTickets} from "@/data";
+
+
+type TicketPageProps = {
+    params: Promise<{
+        ticketId: string
+    }>
+}
+
+const TicketPage = async ({params}: TicketPageProps) => {
+    const {ticketId} = await params;
+
+    const ticket = initialTickets.find((ticket) =>
+        (ticketId === ticket.id))
+
+    if (!ticket) {
+        return (
+            <div>Ticket not found</div>
+        )
+    }
+
+    return (
+        <div>
+            <h2 className='text-lg'>{ticket?.title}</h2>
+            <p>{ticket?.content}</p>
+        </div>
+    )
+}
+
+export default TicketPage;
+
+
